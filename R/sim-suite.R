1f91d015772e500d8d1119b5ec808190adf4943f
Nathanael Aff <nathanaelaff@gmail.com>
2017-06-19 19:56:43 -0700

    sim suite

--- R/sim-suite.R
+++ R/sim-suite.R
@@ -18,19 +18,21 @@ sim_suite <- function(){
   log1 <- logistic(r = 3.87)
   log2 <- logistic(r = 3.70)
 
-  mg1 <- mackeyglass(tau = 27, 
-                     beta = 0.3, 
-                     gamma = 0.1,
-                     n = 9, 
-                     init = rep(0.2),
-                     noise = 1)
+  cauchy1 <- cauchy(alpha = 0.3, beta = 0.5) 
+  cauchy2 <- cauchy(alpha = 0.4, beta = 0.7)
+  # mg1 <- mackeyglass(tau = 27, 
+  #                    beta = 0.3, 
+  #                    gamma = 0.1,
+  #                    n = 9, 
+  #                    init = rep(0.2),
+  #                    noise = 1)
 
-  mg2 <- mackeyglass(tau = 17, 
-                     beta = 0.35, 
-                     gamma = 0.2,
-                     n = 8, 
-                     init = rep(0.2),
-                     noise = 1)
+  # mg2 <- mackeyglass(tau = 17, 
+  #                    beta = 0.35, 
+  #                    gamma = 0.2,
+  #                    n = 8, 
+  #                    init = rep(0.2),
+  #                    noise = 1)
 
   weier1 <- weierstrass(a = 0.4, b = 4)
   weier2 <- weierstrass(a = 0.8, b = 4)
@@ -41,15 +43,17 @@ sim_suite <- function(){
   group1 <- list(ARMA1 = arma1, 
                  # arma2 = arma2, k
                  Logistic1     = log1, 
-                 Weierstrass1  = weier1, 
-                 Mackey_Glass1 = mg1, 
+                 Weierstrass1  = weier1,
+                 cauchy1       = cauchy1, 
+                 # Mackey_Glass1 = mg1, 
                  FARIMA1       = farima1, 
                  fBm1          = fbm1)
   group2 <- list(ARMA2 = arma2, 
                  # arma2 = arma2, k
                  Logistic2     = log2, 
-                 Weierstrass2  = weier2, 
-                 Mackey_Glass2 = mg2, 
+                 Weierstrass2  = weier2,
+                 Cauchy1       = cauchy1,
+                 # Mackey_Glass2 = mg2, 
                  FARIMA2       = farima2,
                  fBm2          = fbm2)
 
@@ -63,5 +67,5 @@ sim_suite <- function(){
 #' @return  A vector of function names
 #' @export
 sim_names <- function() {
-  c("ARMA", "Logistic", "Weierstrass", "Mackey_Glass", "FARIMA", "fBm")
+  c("ARMA", "Logistic", "Weierstrass", "Cauchy", "FARIMA", "fBm")
 }
